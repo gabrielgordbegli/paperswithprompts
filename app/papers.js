3a9a13fe51160c2e321dec347cd54fed17b33de4
gabrielgordbegli <ggordbegli@gmail.com>
2023-04-25 05:25:17 -0400

    added 2303.12712

--- app/papers.js
+++ app/papers.js
@@ -2,17 +2,22 @@ export const papers = [
     {
       submitted: '7 Apr 2023',
       title: 'Generative Agents: Interactive Simulacra of Human Behavior',
-      link: 'https://arxiv.org/pdf/2304.03442.pdf'
+      link: 'https://arxiv.org/abs/2304.03442'
+    },
+    {
+        submitted: '22 Mar 2023',
+        title: 'Sparks of Artificial General Intelligence: Early experiments with GPT-4',
+        link: 'https://arxiv.org/abs/2303.12712'
     },
     {
       submitted: '10 Jan 2023',
       title: 'Chain-of-Thought Prompting Elicits Reasoning in Large Language Models',
-      link: 'https://arxiv.org/pdf/2201.11903.pdf'
+      link: 'https://arxiv.org/abs/2201.11903'
     },
     {
       submitted: '15 Feb 2021',
       title: 'Prompt Programming for Large Language Models: Beyond the Few-Shot Paradigm',
-      link: 'https://arxiv.org/pdf/2102.07350v1.pdf'
-    }
+      link: 'https://arxiv.org/abs/2102.07350'
+    },
   ];
   
