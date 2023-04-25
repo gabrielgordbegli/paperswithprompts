f12f5d5bc5451e0e3ce4febc19cbb5c3909cc9ba
gabrielgordbegli <ggordbegli@gmail.com>
2023-04-25 05:07:49 -0400

    moved papers to their own file

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,9 @@
+import { papers } from './papers';
 import Table from './Table'
-import Header from './Header'
-
-const papers = [
-  {
-      submitted: '7 Apr 2023',
-      title: 'Generative Agents: Interactive Simulacra of Human Behavior',
-      link: 'https://arxiv.org/pdf/2304.03442.pdf'
-  },
-  {
-      submitted: '10 Jan 2023',
-      title: 'Chain-of-Thought Prompting Elicits Reasoning in Large Language Models',
-      link: 'https://arxiv.org/pdf/2201.11903.pdf'
-  },
-  {
-      submitted: '15 Feb 2021',
-      title: 'Prompt Programming for Large Language Models: Beyond the Few-Shot Paradigm',
-      link: 'https://arxiv.org/pdf/2102.07350v1.pdf'
-  }
-  // More people...
-]
 
 export default function Home() {
   return (
     <main className="flex min-h-screen min-w-screen">
-
       <Table papers={papers}/>
     </main>
   )
---
+++ app/papers.js
@@ -0,0 +1,18 @@
+export const papers = [
+    {
+      submitted: '7 Apr 2023',
+      title: 'Generative Agents: Interactive Simulacra of Human Behavior',
+      link: 'https://arxiv.org/pdf/2304.03442.pdf'
+    },
+    {
+      submitted: '10 Jan 2023',
+      title: 'Chain-of-Thought Prompting Elicits Reasoning in Large Language Models',
+      link: 'https://arxiv.org/pdf/2201.11903.pdf'
+    },
+    {
+      submitted: '15 Feb 2021',
+      title: 'Prompt Programming for Large Language Models: Beyond the Few-Shot Paradigm',
+      link: 'https://arxiv.org/pdf/2102.07350v1.pdf'
+    }
+  ];
+  
