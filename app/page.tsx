032c8d1f4322805885a0e1950044fbd2934610e6
gabrielgordbegli <ggordbegli@gmail.com>
2023-04-25 06:14:30 -0400

    added link to github

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
 import { papers } from './papers';
 import Table from './Table'
+import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen min-w-screen">
-      <Table papers={papers}/>
+      <Link href="https://github.com/gabrielgordbegli/paperswithprompts">
+        <div className="absolute right-0 top-0 p-4">
+          <Image src="/github.svg" alt="GitHub Logo" width={50} height={50} />
+        </div>
+      </Link>
+      <Table papers={papers} />
     </main>
   )
 }
--- app/papers.js
+++ app/papers.js
@@ -24,4 +24,29 @@ export const papers = [
         title: 'Prompt Programming for Large Language Models: Beyond the Few-Shot Paradigm',
         link: 'https://arxiv.org/abs/2102.07350'
     },
+    {
+        submitted: '7 Apr 2023',
+        title: 'Generative Agents: Interactive Simulacra of Human Behavior',
+        link: 'https://arxiv.org/abs/2304.03442'
+    },
+    {
+        submitted: '22 Mar 2023',
+        title: 'Sparks of Artificial General Intelligence: Early experiments with GPT-4',
+        link: 'https://arxiv.org/abs/2303.12712'
+    },
+    {
+        submitted: '10 Jan 2023',
+        title: 'Chain-of-Thought Prompting Elicits Reasoning in Large Language Models',
+        link: 'https://arxiv.org/abs/2201.11903'
+    },
+    {
+        submitted: '5 Oct 2022',
+        title: 'Ask Me Anything: A simple strategy for prompting language models',
+        link: 'https://arxiv.org/abs/2210.02441v3'
+    },
+    {
+        submitted: '15 Feb 2021',
+        title: 'Prompt Programming for Large Language Models: Beyond the Few-Shot Paradigm',
+        link: 'https://arxiv.org/abs/2102.07350'
+    },
 ];
